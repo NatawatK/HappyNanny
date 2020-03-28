7daae980c81c9840ae039d0d75a93649b7c2508a
Natawat Kwanpoom <natawat.nueng@gmail.com>
2020-03-28 14:57:56 +0700

    Add login, register page and firebase auth

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,15 +2,29 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import vuetify from './plugins/vuetify';
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
-import '@mdi/font/css/materialdesignicons.css'
+import vuetify from "./plugins/vuetify";
+import "roboto-fontface/css/roboto/roboto-fontface.css";
+import "@mdi/font/css/materialdesignicons.css";
+const fb = require("./firebaseConfig");
 
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
-}).$mount("#app");
+// new Vue({
+//   router,
+//   store,
+//   vuetify,
+//   render: h => h(App)
+// }).$mount("#app");
+
+let app;
+fb.auth.onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      el: "#app",
+      router,
+      vuetify,
+      store,
+      render: h => h(App)
+    });
+  }
+});
