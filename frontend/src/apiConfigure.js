3f364560a84949c29fb8da37b702ab3e1c5d4129
gems.sirawit <sirawit.gems@wongnai.com>
2020-03-29 18:49:07 +0700

    add development environment feature

--- frontend/src/apiConfigure.js
+++ frontend/src/apiConfigure.js
@@ -1,2 +1,3 @@
 export const BACKEND_PATH =
-  process.env.ENV === "production" ? "/api" : "http://localhost/api";
+  // http://localhost:9900 can replace with "/api" or "backend" if run with docker-compose
+  process.env.NODE_ENV === "production" ? "/api" : "http://localhost:9900";
