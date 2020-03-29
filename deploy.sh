cb50d0848d7466c4dbb5d81f5a5697591d6ecafd
gems.sirawit <sirawit.gems@wongnai.com>
2020-03-29 22:40:08 +0700

    fix pipeline for backend

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 
 REPO=569663662309.dkr.ecr.ap-southeast-1.amazonaws.com \
     && aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin $REPO \
-    && docker build --no-cache -t happynanny_$1:latest $1/ \
+    && docker-compose -f docker-compose.yml -f production.yml build --no-cache $1 \
     && docker tag happynanny_$1:latest $REPO/happynanny_$1:latest && docker push $REPO/happynanny_$1:latest \
     && aws ecs update-service --force-new-deployment --service happynanny-service --cluster happynanny-cluster
