1ca7abc628796c6de59e943628ff038e15976cc0
Natawat Kwanpoom <natawat.nueng@gmail.com>
2020-03-31 16:58:25 +0700

    add deploy command

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -10,17 +10,17 @@ import "firebase/firestore";
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyB9TNUobFvMuCcOYu35-0oWwSqCrLrNvis",
-    authDomain: "happy-nanny-11d71.firebaseapp.com",
-    databaseURL: "https://happy-nanny-11d71.firebaseio.com",
-    projectId: "happy-nanny-11d71",
-    storageBucket: "happy-nanny-11d71.appspot.com",
-    messagingSenderId: "626590299693",
-    appId: "1:626590299693:web:1b9a9247704fcb4e576aa4",
-    measurementId: "G-QG1RB4Y86S"
-  };
-  
-  // Initialize Firebase
+  apiKey: "AIzaSyB9TNUobFvMuCcOYu35-0oWwSqCrLrNvis",
+  authDomain: "happy-nanny-11d71.firebaseapp.com",
+  databaseURL: "https://happy-nanny-11d71.firebaseio.com",
+  projectId: "happy-nanny-11d71",
+  storageBucket: "happy-nanny-11d71.appspot.com",
+  messagingSenderId: "626590299693",
+  appId: "1:626590299s693:web:1b9a9247704fcb4e576aa4",
+  measurementId: "G-QG1RB4Y86S"
+};
+
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-export default firebase
+export default firebase;
