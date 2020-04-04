9b0e52bf0443cf7ef6b4c67bd8fffb4d26f1bb91
NatawatK <natawat.nueng@gmail.com>
2020-04-04 22:04:37 +0700

    Add auth api (#5)
    * test api call
    
    * Add AuthToken Header in Axios request

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import UserRepository from '../repository/UserRepository';
 
 export default {
   name: "Home",
   components: {
     HelloWorld
+  },
+  created() {
+    const uid = localStorage.getItem('uid')
+    UserRepository.get(uid).then(res => {
+      console.log(res.data)
+      this.channels = res.data.channels.map( e => ({ icon: "mdi-contacts", text : e }))
+    })
   }
 };
 </script>
